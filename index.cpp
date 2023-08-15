2ff9de2ceed075542b6289bcbdcef0d5c4a5dada
havelll887 <aoyue1122@outlook.com>
2023-08-15 23:50:48 +0800

    some change

---
+++ index.cpp
@@ -0,0 +1,22 @@
+// 封装函数显示该界面，如 void showMenu()
+// 在main函数中调用封装好的函数
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+// 菜单界面
+void showMenu()
+{
+	cout << "sdnbikurfjgvher" << endl;
+	cout << "nhiehnfw" << endl;
+	cout << "nihadfa " << endl;
+
+}
+
+int main() 
+{
+	showMenu();
+	system("pause");
+	return 0;
+}
