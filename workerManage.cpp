8767cb1901a099b6694708a781a50914a6e63018
havelll887 <aoyue1122@outlook.com>
2023-09-05 00:08:01 +0800

    some change

--- workerManage.cpp
+++ workerManage.cpp
@@ -2,7 +2,11 @@
 
 WorkerMange::WorkerMange()
 {
+	//初始化人数
+	this->m_EmpNum = 0;
 
+	//初始化数组指针
+	this->m_EmpArray = NULL;
 };
 
 void WorkerMange::Show_Menu()
@@ -27,6 +31,29 @@ void WorkerMange::ExitSystem()
 	cout << "欢迎下次使用" << endl;
 	system("pause");
 	exit(0);
+}
+
+//增加职工
+void WorkerMange::Add_Emp()
+{
+	cout << "请输入增加职工数量： " << endl;
+
+	// 保存用户的输入数量
+	int addNum = 0;
+	cin >> addNum;
+
+	if (addNum > 0)
+	{
+		//计算新空间大小，新的空间大小 = 原来记录人数 + 新增人数
+		int newSize = this->m_EmpNum + addNum;
+	}
+	else 
+	{
+		cout << "输入有误" << endl;
+	}
+
+	system("pause");
+	system("cls");
 }
 
 WorkerMange::~WorkerMange()
--- workerManage.h
+++ workerManage.h
@@ -1,6 +1,7 @@
 #pragma once  // 防止头文件重复包含
 #include <iostream> // 包含输入输出流头文件
 using namespace std; // 使用标准的命名空间
+#include "worker.h"
 
 class WorkerMange
 {
@@ -14,6 +15,15 @@ public:
 
 	// 退出系统
 	void ExitSystem();
+
+	//记录文件中的人数个数
+	int m_EmpNum;
+
+	//员工数组的指针
+	Worker ** m_EmpArray;
+
+	//增加职工
+	void Add_Emp();
 
 	// 析构函数
 	~WorkerMange();
